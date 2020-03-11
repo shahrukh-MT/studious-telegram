9cd30dbca492bb1e4e487e72edc6a8e87f3fd2c0
Shahrukh MdAdil <mdadil.shahrukh@mindtickle.com>
2020-03-11 13:48:09 +0530

    Updates

---
+++ precache-manifest.e5bd1eab4f522c769809a92829e5a084.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7977b94dfb866c3417b0f60e684406",
+    "url": "/studious-telegram/index.html"
+  },
+  {
+    "revision": "6a2a60d95da15d8cef53",
+    "url": "/studious-telegram/static/css/main.861fc9ef.chunk.css"
+  },
+  {
+    "revision": "65bf959be831c4b29443",
+    "url": "/studious-telegram/static/js/2.80f7d978.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/studious-telegram/static/js/2.80f7d978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a2a60d95da15d8cef53",
+    "url": "/studious-telegram/static/js/main.e2c99bf0.chunk.js"
+  },
+  {
+    "revision": "76620652cf116da65ca5",
+    "url": "/studious-telegram/static/js/runtime-main.8a470c77.js"
+  },
+  {
+    "revision": "76805bc81807a9a3c048495d8fe76ae5",
+    "url": "/studious-telegram/static/media/bg_with_Land.76805bc8.png"
+  }
+]);
